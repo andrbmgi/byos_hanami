1ec3a328eef6ce5d9c80b93567bc3bb727c6a008
i534091 <andreas.raede@sap.com>
2026-01-16 16:37:51 +0100

    update

--- app/actions/api/screens/patch.rb
+++ app/actions/api/screens/patch.rb
@@ -25,8 +25,8 @@ module Terminus
               optional(:label).filled :string
               optional(:name).filled :string
               optional(:content).filled :string
-              optional(:uri).filled :string
-              optional(:preprocessed).filled :bool
+              optional(:uri) { filled? > str? }
+              optional(:preprocessed) { bool? }
             end
           end
 
@@ -51,11 +51,6 @@ module Terminus
           end
 
           def update screen, parameters
-            # Debug: log what we're receiving
-            logger.info "PATCH parameters: #{parameters.to_h.inspect}"
-            logger.info "Has uri key? #{parameters.key?(:uri)}, value: #{parameters[:uri].inspect}"
-            logger.info "Has preprocessed key? #{parameters.key?(:preprocessed)}, value: #{parameters[:preprocessed].inspect}"
-            
             if parameters.key? :content
               merge(screen, parameters).bind { |attributes| build_mold attributes }
                                        .bind { |instance| screenshot instance, screen, parameters }
