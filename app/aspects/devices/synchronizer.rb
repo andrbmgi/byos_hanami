3a4d0d1e987fa87de82520ed140a89302381b911
i534091 <andreas.raede@sap.com>
2026-01-19 21:36:11 +0100

    log

--- app/aspects/devices/synchronizer.rb
+++ app/aspects/devices/synchronizer.rb
@@ -10,6 +10,7 @@ module Terminus
       class Synchronizer
         include Deps[
           :settings,
+          :logger,
           firmware_parser: "aspects.firmware.header",
           repository: "repositories.device"
         ]
@@ -22,9 +23,24 @@ module Terminus
 
         def update result
           result.bind do |payload|
+            logger.info "Looking up device by MAC address",
+                       mac_address: payload.mac_address,
+                       device_attributes: payload.device_attributes
+            
             device = repository.update_by_mac_address payload.mac_address,
                                                       **payload.device_attributes
-            device ? Success(device) : Failure("Unable to find device by MAC address.")
+            
+            if device
+              logger.info "Device found and updated",
+                         device_id: device.id,
+                         device_name: device.name,
+                         mac_address: device.mac_address
+              Success(device)
+            else
+              logger.error "Device not found in database",
+                          mac_address: payload.mac_address
+              Failure("Unable to find device by MAC address.")
+            end
           end
         end
       end
